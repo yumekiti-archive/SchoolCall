91adb7e1793622283196b699339f21cbf6edf923
yumekiti <yumekiti120440@gmail.com>
2023-05-06 13:56:13 +0900

    `change` api host

--- libs/fetchInstance.ts
+++ libs/fetchInstance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchInstance = () => {
   return axios.create({
-    baseURL: 'http://192.168.11.3:3000/api',
+    baseURL: 'http://localhost:3000/api',
     headers: {
       'Content-Type': 'application/json',
     },
