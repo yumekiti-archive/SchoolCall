a8f01292b50ed00e6665eadffbe579b04594a21c
yumekiti <yumekiti120440@gmail.com>
2023-05-11 12:35:28 +0900

    `update` 文字大きくした

--- components/molecules/DeskTable.tsx
+++ components/molecules/DeskTable.tsx
@@ -11,19 +11,19 @@ type Props = {
 
 const DeskTable: FC<Props> = ({ seat_number, name, connect = false, turn_num, status, today }) => {
   return seat_number === 0 && name === undefined ? (
-    <td className='h-full row-span-1'>&nbsp;</td>
+    <td className='h-full row-span-1 text-2xl'>&nbsp;</td>
   ) : (
     <td className='w-full h-full row-span-1'>
       {!connect ? (
         <div className='w-full h-full flex'>
           <div
-            className={`w-10/12 h-full flex justify-start items-end pl-2 border border-gray-400 relative
+            className={`w-10/12 h-full flex justify-start items-end pl-2 border border-gray-400 relative text-2xl
             ${ turn_num ? 'bg-green-200' : '' }
             ${ today ? 'bg-gray-200' : '' }
             `}
           >
             {name ? name : <>&nbsp;</>}
-            <p className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl font-bold text-gray-700'>
+            <p className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-6xl font-bold text-gray-700'>
               {turn_num ? turn_num : ''}
             </p>
           </div>
@@ -37,14 +37,14 @@ const DeskTable: FC<Props> = ({ seat_number, name, connect = false, turn_num, st
             <p className='text-right'>{seat_number === 0 || !seat_number ? <>&nbsp;</> : seat_number}</p>
           </div>
           <div
-            className={`w-10/12 h-full flex justify-start items-end pl-2 border border-gray-400 relative
+            className={`w-10/12 h-full flex justify-start items-end pl-2 border border-gray-400 relative text-2xl
             ${ !status && turn_num ? 'bg-green-200' : '' }
             ${ today ? 'bg-gray-200' : '' }
             `}
           >
             <div>{name ? name : <>&nbsp;</>}</div>
 
-            <p className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl font-bold text-gray-700'>
+            <p className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-6xl font-bold text-gray-700'>
               {!status && turn_num ? turn_num : ''}
             </p>
           </div>
